f803f4a026c1f130cfdbc830c9c1b47743ba3537
andre <andre@andre-IPMH110P>
2021-01-31 13:57:29 -0300

    teste

--- src/main/java/br/com/folha/FolhaWebApplication.java
+++ src/main/java/br/com/folha/FolhaWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FolhaWebApplication {
+public class FolhaWebApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(FolhaWebApplication.class, args);
